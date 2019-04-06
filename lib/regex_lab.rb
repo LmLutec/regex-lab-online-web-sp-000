ce75165f504833378cc4a325971f538fc68dee44
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-06 21:08:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,7 +26,7 @@ end
 
 def valid_phone_number?(phone)
  binding.pry 
-  if phone.match(/[0-9]{10}|\(*[0-9]{3}[\)-]*[0-9]{3}[-\s]*[0-9]{4}/)
+  if phone.match(/[0-9]{10}|\(*[0-9]{3}[\)-\s]*[0-9]{3}[-\s]*[0-9]{4}/)
     true 
   else 
     false 
